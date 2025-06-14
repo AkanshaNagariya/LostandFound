2a4769a79a4e6780f205391d60ad31fdf1dd9372
Akansha Nagariya <145829960+AkanshaNagariya@users.noreply.github.com>
2025-06-14 19:07:08 +0530

    Delete laf-backend directory

--- laf-backend/src/main/java/lostandfound/laf_backend/repository/ClaimRepository.java
+++
@@ -1,8 +0,0 @@
-package lostandfound.laf_backend.repository;
-import lostandfound.laf_backend.Model.Claim; // Import your Claim entity
-import lostandfound.laf_backend.Model.Enums.ClaimStatus; // Import ClaimStatus enum
-import org.springframework.data.jpa.repository.JpaRepository; // Add this import
-import java.util.List;
-public interface ClaimRepository extends JpaRepository<Claim, Long> {
-    List<Claim> findByStatus(ClaimStatus status);
-}
